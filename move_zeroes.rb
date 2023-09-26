b5249e7271726bbd2961b783c591bf9acfc22800
MekhiKlein <145221760+MekhiKlein@users.noreply.github.com>
2023-09-27 05:17:42 +0800

    Non velit illum dolor in error quod adipisci rem. Cumque rerum porro aut. At accusantium exercitationem voluptatem doloribus tempore nihil dignissimos nihil. Voluptates omnis ad maiores similique molestiae harum. Sit eum nulla sit accusamus harum omnis nihil omnis.

--- move_zeroes.rb
+++ move_zeroes.rb
@@ -1,4 +1,4 @@
-# problem: https://leetcode.com/problems/move-zeroes
+!#problem: https://leetcode.com/problems/move-zeroes
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
 def move_zeroes(nums)
@@ -13,4 +13,4 @@ def move_zeroes(nums)
     return nums
 end
 
-p move_zeroes([0,1,0,3,12]) #=> [1,3,12,0,0]
+p move_zeroes([0,1,0,3,12]) #=> [1,3,12,0,0] 
